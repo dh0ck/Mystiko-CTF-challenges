efa4dccef08dac131d5a7a865ca11827f5e8246f
dh0ck <k0n4nd484r84r14n@gmail.com>
2021-12-08 11:56:23 +0100

    first upload

---
+++ Hell Yeah!/goto.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char answer;
+
+    while(true){
+    cout << "do you want to see the flag? (y/n)" << endl;
+    cin >> answer;
+    if (answer == 'y'){
+        cout << "Try harder" << endl;;
+    }
+    else{
+        cout << "Ok" << endl;
+        exit(0);
+    }
+    }
+
+    cout << "M" << endl;
+    cout << "y" << endl;
+    cout << "s" << endl;
+    cout << "t" << endl;
+    cout << "i" << endl;
+    cout << "k" << endl;
+    cout << "o" << endl;
+    cout << "{" << endl;
+    cout << "$" << endl;
+    cout << "3" << endl;
+    cout << "3" << endl;
+    cout << "_" << endl;
+    cout << "Y" << endl;
+    cout << "0" << endl;
+    cout << "u" << endl;
+    cout << "_" << endl;
+    cout << "1" << endl;
+    cout << "N" << endl;
+    cout << "_" << endl;
+    cout << "H" << endl;
+    cout << "3" << endl;
+    cout << "L" << endl;
+    cout << "L" << endl;
+    cout << "}" << endl;
+}
